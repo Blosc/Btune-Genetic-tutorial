78787cc7e77454a4ee0a005055a90de63ef1c3b8
Francesc Alted <francesc@blosc.org>
2023-12-02 14:05:11 +0100

    Added a new compression benchmark

---
+++ compr_bench.py
@@ -0,0 +1,71 @@
+#######################################################################
+# Copyright (c) 2019-present, Blosc Development Team <blosc@blosc.org>
+# All rights reserved.
+#
+# This source code is licensed under a BSD-style license (found in the
+# LICENSE file in the root directory of this source tree)
+#######################################################################
+import numpy as np
+import blosc2
+from time import time
+
+
+# Create the NDArray
+rng = np.random.default_rng()
+print("Creating data for genetic purposes...")
+a = rng.integers(low=0, high=10000, size=int(4e8), dtype=np.int64)
+
+# Compress
+cparams = dict()
+chunks, blocks = None, None
+#cparams = dict(codec=blosc2.Codec.LZ4, clevel=9, filters=[blosc2.Filter.SHUFFLE], splitmode=blosc2.SplitMode.ALWAYS_SPLIT)
+#cparams = dict(codec=blosc2.Codec.ZSTD, clevel=1, filters=[blosc2.Filter.SHUFFLE], splitmode=blosc2.SplitMode.ALWAYS_SPLIT)
+#cparams = dict()
+#chunks, blocks = (4194304,), (524288,)
+t0 = time()
+b = blosc2.asarray(a, cparams=cparams, chunks=chunks, blocks=blocks)
+schunk = b.schunk
+t = time() - t0
+print(f"NDArray created! {schunk.cratio:.2f}x, cspeed={schunk.nbytes / t / 1e6:.2f} MB/s"
+      f"\n size={schunk.nbytes / 1e6:.2f} MB, csize={schunk.cbytes / 1e6:.2f} MB"
+      f"\n time={t:.2f} s"
+      f"\n{b.info}")
+
+# Compress chunk by chunk
+# t = 0
+# for chunk in b.schunk.iterchunks(dtype=b.dtype):
+#     t0 = time()
+#     _ = blosc2.compress2(chunk, cparams=cparams)
+#     #b_ = blosc2.asarray(chunk, cparams=cparams)
+#     t += time() - t0
+# print(f"Compressed chunk by chunk! {t:.2f} s, cspeed={b.schunk.nbytes / t / 1e6:.2f} MB/s")
+
+# Decompress
+t0 = time()
+c = b[:]
+t = time() - t0
+print(f"NDarray decompressed in one go: {t:.2f} s, dspeed={b.schunk.nbytes / t / 1e6:.2f} MB/s")
+
+# Sum NumPy
+t0 = time()
+sum1 = c.sum()
+t = time() - t0
+print(f"Summed NumPy in {t:.2f} s, speed={b.schunk.nbytes / t / 1e6:.2f} MB/s")
+
+# Decompress chunk by chunk
+t0 = time()
+for chunk in b.schunk.iterchunks(dtype=b.dtype):
+    pass
+t = time() - t0
+print(f"Decompressed chunk by chunk: {t:.2f} s, dspeed={b.schunk.nbytes / t / 1e6:.2f} MB/s")
+
+# Decompress and sum chunk by chunk
+t0 = time()
+sum2 = 0
+for chunk in b.schunk.iterchunks(dtype=b.dtype):
+    sum2 += chunk.sum()
+t = time() - t0
+print(f"Summed and decompressed chunk by chunk: {t:.2f} s, dspeed={b.schunk.nbytes / t / 1e6:.2f} MB/s")
+
+assert sum1 == sum2
+print("Decompression and checksum OK!")
